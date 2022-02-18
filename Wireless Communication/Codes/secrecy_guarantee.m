4996113ec3757e498e3c12fc9ceac80abdfa2bb3
Venissa Adzo Sedem Manya <96427027+vasmanya@users.noreply.github.com>
2022-02-18 13:20:51 +0900

    Update secrecy_guarantee.m

--- Wireless Communication/Codes/secrecy_guarantee.m
+++ Wireless Communication/Codes/secrecy_guarantee.m
@@ -38,31 +38,7 @@ for j = 1:iterations
  plot(N_r,Capacity,'r:d');
  ylim([0 25])
 
-% 
-% 
-%     %Water filling algorithm for power allocation
-%     for  i = 1:length(SNR_dB)
-%          P_t = 10.^(SNR_dB(i)/10);   % Assuming noise power to be equal to 1
-%          [U, S, V] = svd(H_H);
-%          C_k = diag(S); % channel state information given by |h_k|/noise power^2
-%          g_lambda_mu = 1e4; %number of points to compute lagrangian dual function
-%          mu = linspace(0.7,0.698,g_lambda_mu);
-%          allocated_power = max(1./mu-(1./C_k), 0); %x^+ = max(x,0)
-% 
-%          %Lagrangian function
-%          g_mu = sum(log(1+(allocated_power.*(repmat(C_k,[1,g_lambda_mu]))))) - mu.*((sum(allocated_power))-P_t);
-%          [min_g_mu, Ind] = find(g_mu == min(g_mu));
-%          optimal_mu = mu(Ind);
-%          optimal_power = max(1./optimal_mu - 1./C_k, 0);
-%          Q_opt = diag(optimal_power);
-%          Capacity(i) = Capacity(i) + log(real(det(I_N_R + (P_t/N_t)*Q_opt*H_H)));
-%     end
-% end
-% 
-% Capacity = Capacity/iterations;
-% 
-%
-% 
+
 
 
 
